76419fd4f2ac67519a1f402d093612cad59294c4
Chepynets Andrii <chepynets.andrii@gmail.com>
2025-07-19 23:19:08 +0300

    Added weapon assets, melee and ranged player animations

--- Assets/Plugins/SupanthaPaul/2D Platformer Controller/Scripts/Player/PlayerAnimator.cs
+++ Assets/Plugins/SupanthaPaul/2D Platformer Controller/Scripts/Player/PlayerAnimator.cs
@@ -13,12 +13,13 @@ namespace SupanthaPaul
 		private static readonly int WallGrabbing = Animator.StringToHash("WallGrabbing");
 		private static readonly int IsDashing = Animator.StringToHash("IsDashing");
 
-		private void Start()
+
+        private void Start()
 		{
 			m_anim = GetComponentInChildren<Animator>();
 			m_controller = GetComponent<PlayerController>();
 			m_rb = GetComponent<Rigidbody2D>();
-		}
+        }
 
 		private void Update()
 		{
